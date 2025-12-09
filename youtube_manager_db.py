e06cea274e7f645d4daf438d40921f5e3add15ac
muhammadkhan-dev-code <muhammadkhansw00@gmail.com>
2025-12-09 23:34:49 +0500

    youtube_manager with database

--- youtube_manager_db.py
+++ youtube_manager_db.py
@@ -12,7 +12,29 @@ cursor.execute('''
 
 ''')
 
+def list_videos():
+    print("\n","★ "* 5 ," Youtube Videos ", "★ "* 5)  
+    cursor.execute("SELECT * FROM videos")
+    for row in cursor.fetchall():
+        print(f'ID: {row[0]}, Name: {row[1]}, Time: {row[2]}')
+    
+    print( "\t","★ "* 10)
 
+
+def add_video(name, time):
+    cursor.execute("INSERT INTO videos(name,time) VALUES(?,?)",(name,time))
+    connection.commit()
+
+
+def update_video(vidId, new_name, new_time):
+    cursor.execute("UPDATE videos SET name=?, time=? WHERE id=?", (new_name, new_time, vidId))
+    connection.commit()
+
+
+def delete_video(vidId):
+    cursor.execute("DELETE FROM videos WHERE id=?", (vidId,))
+    connection.commit()
+    
 def main():
     while True:
         print("\nYouTube  Manager App With DB")
@@ -21,9 +43,29 @@ def main():
         print("3 update Video")
         print("4. Delete Video")
         print("5. Exit")
-        choice = input("Enter your choice: ")
-        
+        choice = input("\n ★ Enter your choice: ")
 
+        if choice == '1':
+            list_videos()
+        elif choice == '2':
+            name= input("Enter the video name: ")
+            time=input("Enter the video time: ")
+            add_video(name,time)
+        elif choice == '3':
+            vidId= input ("Enter video Id to update: ")
+            name= input("Enter the new video name: ")
+            time=input("Enter the new video time: ")
+            update_video(vidId, name , time)
+        elif choice == '4':
+            vidId= input ("Enter video Id to delete:")
+            delete_video(vidId)
+        elif choice == '5':
+            print("\n ★ Thanks for Using Youtube  Video Manager ★")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+    
+    connection.close()
 
 if __name__ == '__main__':
     main()
